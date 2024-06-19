de16db81aaaa3f6d509cc34019d96da65890d1b2
茶子枫 <1511608962@qq.com>
2024-06-19 15:41:05 +0800

    定义了一个 HTTPPool 结构体及其相关方法，通过 HTTP 接口处理缓存节点间的通信，提供日志记录和缓存数据的获取功能。通过解析 HTTP请求路径，获取缓存组和键值，并返回相应的缓存数据。

---
+++ src/geecache/http.go
@@ -0,0 +1,62 @@
+package geecache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_geecache/"
+
+type HTTPPool struct {
+	//本机地址 ip+端口
+	self string
+	//节点通讯前缀
+	basePath string
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+// Log 打印日志
+func (p *HTTPPool) Log(format string, v ...any) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+
+	p.Log("%s %s", r.Method, r.URL.Path)
+
+	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
+
+	if len(parts) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := parts[0]
+	key := parts[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteSlice())
+}
